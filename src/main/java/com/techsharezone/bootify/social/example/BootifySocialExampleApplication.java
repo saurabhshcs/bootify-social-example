2e1e67cf2ac23beaf7dae0f9e078a09ffdb3bc92
Saurabh Sharma <saurabhshcs@gmail.com>
2021-02-08 17:43:46 +0000

    bootify-social-001 - saurabhshcs - Initial code commit

---
+++ src/main/java/com/techsharezone/bootify/social/example/BootifySocialExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootify.social.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifySocialExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifySocialExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsharezone/bootify/social/example/BootifySocialExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootify.social.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifySocialExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
